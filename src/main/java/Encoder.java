fc2de4827026041ef9b4c7045ce1fa63f69a3c22
klochkov <klochkov.777@mail.ru>
2022-09-02 00:58:50 +0300

    not finish Encoder before sleepping

---
+++ src/main/java/Encoder.java
@@ -0,0 +1,27 @@
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Encoder {
+    private int key;
+    private Path pathInput;
+    private Path pathOutput;
+
+
+    public Encoder(int key) {
+        this.key = key;
+    }
+
+    public String encodeLine(String line, List<Character> list, List<Character> list2){
+
+        var stringBuilder = new StringBuilder();
+        return new String();
+    }
+
+
+
+
+
+}
---
+++ src/main/java/JavaRushEncoder.java
@@ -0,0 +1,5 @@
+public class JavaRushEncoder {
+    public static void main(String[] args) {
+
+    }
+}
