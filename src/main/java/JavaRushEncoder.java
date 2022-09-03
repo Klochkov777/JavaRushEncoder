73770ecb57615f07176443badffc6cf13f54d56a
klochkov <klochkov.777@mail.ru>
2022-09-03 04:06:41 +0300

    started create Reviser

--- src/main/java/JavaRushEncoder.java
+++ src/main/java/JavaRushEncoder.java
@@ -1,18 +1,30 @@
+import additional.Reviser;
+import alphabets.Alphabet;
 import alphabets.EnglishAlphabet;
 import encoders.Encoder;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class JavaRushEncoder {
     public static void main(String[] args) {
-//        if (!isToEncodeAndCorrectOtherArgs(args)){
-//
-//        }
+        Reviser reviser = new Reviser(args);
+        EnglishAlphabet alphabet = new EnglishAlphabet();
+       if (!reviser.isArgsCorrect()) return;
+       if (reviser.isNeedEncode()){
+           if (reviser.isExistDecodedFileWhenEncode()) {return;}
+           else {createEncodedFile(args, alphabet);}
+       }
+       if ((reviser.isNeedDecode())){
+
+
+
+
+
+       }
     }
 
 
-    public static void creteEncodedFile(String[] args){
+    public static void createEncodedFile(String[] args, Alphabet alphabet){
         EnglishAlphabet englishAlphabet = new EnglishAlphabet();
         Path pathInputFile = Path.of(args[1]);
             Encoder encoderCesar = new Encoder(pathInputFile, Integer.parseInt(args[2]));
@@ -21,26 +33,5 @@ public class JavaRushEncoder {
 
     }
 
-    private static boolean isToEncodeAndCorrectOtherArgs(String[] args){
-        Path pathInputFile = Path.of(args[1]);
-        if (!args[0].equalsIgnoreCase("encoded")) {
-            return false;
-        }if (Files.notExists(pathInputFile)){
-            System.out.println("File for encoding does not exist");
-            return false;
-        }
-        if (!Files.notExists(Path.of(args[1] + "encoded"))){
-            System.out.println("File is exist with such name as we are creating. " +
-                    "Please delete file and then restart application");
-            return false;
-        }
-        char[] arr = args[2].toCharArray();
-        for (char c : arr) {
-           if (!Character.isDigit(c)) {
-               System.out.println("Third arg must be number, please restart application with correct arg.");
-               return false;
-           }
-        }
-        return true;
-    }
+
 }
--- src/main/java/additional/Reviser.java
+++ src/main/java/additional/Reviser.java
@@ -1,25 +1,60 @@
 package additional;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Reviser {
     String[] args;
     public Reviser(String[] args) {
         this.args = args;
     }
 
-    public boolean firstArg(){
-        if (args[0].equalsIgnoreCase("encoded") || args[0].equalsIgnoreCase("decoded")){
+    public boolean isArgsCorrect(){
+        return (isFirstArgCorrect() && isSecondArgCorrect() && isThirdArgCorrect());
+    }
+
+    public boolean isFirstArgCorrect(){
+        if (!args[0].equalsIgnoreCase("encode") && !args[0].equalsIgnoreCase("decode")){
             System.out.println("First arg is not correct. Enter right arg (\"encode/decode\")");
             return false;
         }
         return true;
     }
 
-    public boolean secondArg(){
-        if (args[0].equalsIgnoreCase("encoded") || args[0].equalsIgnoreCase("decoded")){
-            System.out.println("First arg is not correct. Enter right arg (\"encode/decode\")");
+    public boolean isSecondArgCorrect(){
+        Path pathInputFile = Path.of(args[1]);
+        if (Files.notExists(pathInputFile)){
+            System.out.println("File for encoding does not exist, restart application and enter right path");
             return false;
         }
         return true;
+    }
+
+    public boolean isThirdArgCorrect(){
+        char[] arr = args[2].toCharArray();
+        for (char c : arr) {
+            if (!Character.isDigit(c)) {
+                System.out.println("Third arg must be number, please restart application with correct arg.");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isNeedEncode(){
+        return args[0].equalsIgnoreCase("encode");
+    }
+    public boolean isNeedDecode(){
+        return args[0].equalsIgnoreCase("decode");
+    }
+
+    public boolean isExistDecodedFileWhenEncode(){
+        if (!Files.notExists(Path.of(args[1] + "(encoded)"))){
+            System.out.println("File is exist with such name as we are creating. " +
+                    "Please delete file and then restart application");
+            return true;
+        }
+        return false;
     }
 
 
